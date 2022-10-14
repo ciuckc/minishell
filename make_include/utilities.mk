f30a5822dea04208ced94ad8175ff8f1c4c943c4
scristia <scristia@f0r5s15.codam.nl>
2022-10-14 05:09:47 +0200

    Added: Hashmap library to the general makefile

--- make_include/utilities.mk
+++ make_include/utilities.mk
@@ -9,3 +9,11 @@ DIR_LIBFT = $(UTILS)/libft
 NAME_LIBFT = $(DIR_LIBFT)/libft.a
 
 OBJ += $(NAME_LIBFT)
+
+#HASHMAP
+
+DIR_HASH = $(UTILS)/hashmap
+
+NAME_HASH = $(DIR_HASH)/hashmap.a
+
+OBJ += $(NAME_HASH)
