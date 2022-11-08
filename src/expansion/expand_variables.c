e3395fd5e5bd2ddff53fe1e683f08b84f4a6b4b2
scristia <scristia@f0r5s15.codam.nl>
2022-11-08 04:00:16 +0100

    TBD: variable expansion needs rewrite

--- src/expansion/expand_variables.c
+++ src/expansion/expand_variables.c
@@ -6,74 +6,14 @@
 /*   By: scristia <scristia@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/03 02:23:42 by scristia      #+#    #+#                 */
-/*   Updated: 2022/11/08 01:21:42 by scristia      ########   odam.nl         */
+/*   Updated: 2022/11/08 03:59:28 by scristia      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "var_expansion.h"
 
-static ssize_t	st_var_len(char **str, t_table *table)
-{
-	ssize_t	name_len;
-	t_env	vars;
-
-	name_len = 0;
-	(*str)++;
-	if (**str == '\'' || **str == '\"' || **str == '$')
-		return (0);
-	name_len += ft_len_to_char_set(*str, "$\'\"");
-	vars.name = ft_substr(*str, 0, name_len);
-	if (vars.name == NULL)
-		return (-1);
-	*str += name_len;
-	vars.value = item_search(vars.name, table)->data;
-	if (vars.value == NULL)
-		return (0);
-	return (free(vars.name), ft_strlen(vars.value));
-}
-
-static ssize_t	st_get_len_of_expanded_var(char *str, t_table *table)
-{
-	ssize_t	len;
-
-	len = 0;
-	while (*str)
-	{
-		while (*str && ft_strchr("$\"\'", *str))
-			str++;
-		if (*str == '\'')
-		{
-			str++;
-			str += ft_len_to_char_set(str, "\'") + 1;
-		}
-		else if (*str == '\"')
-		{
-			str++;
-			str += ft_len_to_char_set(str, "$\"");
-			if (*str == '\"')
-				str++;
-			else if (*str == '$')
-				len += st_var_len(&str, table);
-		}
-		else if (*str == '$')
-			len += st_var_len(&str, table);
-	}
-	return (len);
-}
-
-static ssize_t	st_get_len_unexpanded(char *str)
-{
-	(void)str;
-	return (0);
-}
-
 static void	st_expand_var_in_str(t_token_list *word, t_table *table)
 {
-	ssize_t	var_len_sum;
-	ssize_t	unexpanded_len;
-
-	var_len_sum = st_get_len_of_expanded_var(word->tok->str, table);
-	unexpanded_len = st_get_len_unexpanded(word->tok->str);
 }
 
 void	expand_variables(t_token_list *words, t_table *table)
