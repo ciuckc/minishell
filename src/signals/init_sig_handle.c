bc648e3183c071ffd44f7510fc92a682c60cd172
scristia <scristia@f0r5s15.codam.nl>
2022-11-17 07:31:04 +0100

    started signals

--- src/signals/init_sig_handle.c
+++ src/signals/init_sig_handle.c
@@ -6,13 +6,24 @@
 /*   By: scristia <scristia@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/17 07:02:55 by scristia      #+#    #+#                 */
-/*   Updated: 2022/11/17 07:09:02 by scristia      ########   odam.nl         */
+/*   Updated: 2022/11/17 07:28:49 by scristia      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	st_handle_interrupt(int signum, siginfo_t *info, void *context)
+{
+}
+
 void	init_sig_handle(void)
 {
+	struct sigaction	sig_inter;
+	struct sigaction	sig_quit;
+	struct sigaction	sig_tstp;
 
+	sig_inter.sa_flags = SA_SIGINFO;
+	sig_quit.sa_flags = SA_SIGINFO;
+	sig_tstp.sa_flags = SA_SIGINFO;
+	sig_inter.sa_sigaction = st_handle_interrupt;
 }
