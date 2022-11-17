65bf163200ecc186bb1f32a4999d07e208da2dd0
scristia <scristia@f0r5s15.codam.nl>
2022-11-17 04:00:28 +0100

    got back the ft_echo into exec

---
+++ src/builtins/ft_echo.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_echo.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: scristia <scristia@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/17 03:58:47 by scristia      #+#    #+#                 */
+/*   Updated: 2022/11/17 03:59:31 by scristia      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../execution_src/execution.h"
+
+int	is_flag_n(char *str)
+{
+	int	i;
+	int	nl;
+
+	i = 0;
+	nl = 0;
+	if (str[i] == '-' && str[i + 1] == 'n')
+	{
+		i++;
+		while (str[i])
+		{
+			if (str[i] == 'n')
+				nl = 1;
+			else
+				nl = 0;
+			i++;
+		}
+	}
+	return (nl);
+}
+
+int	ft_echo(char **arg)
+{
+	int		i;
+	int		is_n;
+
+	i = 1;
+	is_n = 0;
+	if (!arg[i])
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (0);
+	}
+	while (arg[i] && is_flag_n(arg[i]))
+		i++;
+	while (arg[i])
+	{
+		ft_putstr_fd(arg[i], STDOUT_FILENO);
+		if (arg[i + 1])
+			ft_putstr_fd(" ", STDOUT_FILENO);
+		i++;
+	}
+	if (arg[1] && is_flag_n(arg[1]) == 0)
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	return (0);
+}
