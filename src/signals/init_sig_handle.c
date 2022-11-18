a88ca7f23a5cf419f29a2de5768a8ab35538c6ad
scristia <scristia@f0r5s15.codam.nl>
2022-11-18 12:28:53 +0100

    addded sig exit + 128

--- src/signals/init_sig_handle.c
+++ src/signals/init_sig_handle.c
@@ -6,7 +6,7 @@
 /*   By: scristia <scristia@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/17 07:02:55 by scristia      #+#    #+#                 */
-/*   Updated: 2022/11/17 07:28:49 by scristia      ########   odam.nl         */
+/*   Updated: 2022/11/18 12:27:02 by scristia      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 static void	st_handle_interrupt(int signum, siginfo_t *info, void *context)
 {
+	(void)info;
+	(void)context;
+	g_exit_code += signum + 128;
 }
 
 void	init_sig_handle(void)
@@ -26,4 +29,5 @@ void	init_sig_handle(void)
 	sig_quit.sa_flags = SA_SIGINFO;
 	sig_tstp.sa_flags = SA_SIGINFO;
 	sig_inter.sa_sigaction = st_handle_interrupt;
+	sigaction(SIGINT, &sig_inter, NULL);
 }
