9c03e61c58ecafaae2d70bba7bdf8707188e7de5
scristia <scristia@f0r5s15.codam.nl>
2022-12-17 17:20:07 +0100

    norm 2

--- src/execution_src/redir_builtins.c
+++ src/execution_src/redir_builtins.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   redir_builtins.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: emlicame <emlicame@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/11 17:39:53 by emlicame          #+#    #+#             */
-/*   Updated: 2022/12/13 17:09:24 by emlicame         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   redir_builtins.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: emlicame <emlicame@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/12/11 17:39:53 by emlicame      #+#    #+#                 */
+/*   Updated: 2022/12/17 17:19:27 by scristia      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ static int32_t	built_redir_less(t_token *tok, t_input *data)
 	data->fds[READ] = open (tok->next->str, O_RDONLY);
 	if (data->fds[READ] < 0)
 		return (error_print(tok->next->str), 1);
-	// if (dup_and_close(data->fds[READ], STDIN_FILENO) < 0)
-	// 	return (1);
 	return (0);
 }
 
@@ -31,8 +29,6 @@ static int32_t	built_redir_great(t_token *tok, t_input *data)
 	data->fds[WRITE] = open(tok->next->str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (data->fds[WRITE] < 0)
 		return (error_print(tok->next->str), 1);
-	// if (dup_and_close(data->fds[WRITE], STDOUT_FILENO) < 0)
-	// 	return (1);
 	return (0);
 }
 
@@ -43,8 +39,6 @@ static int32_t	built_redir_dgreat(t_token *tok, t_input *data)
 	data->fds[WRITE] = open(tok->next->str, O_RDWR | O_CREAT | O_APPEND, 0644);
 	if (data->fds[WRITE] < 0)
 		return (error_print(tok->next->str), 1);
-	// if (dup_and_close(data->fds[WRITE], STDOUT_FILENO) < 0)
-	// 	return (1);
 	return (0);
 }
 
@@ -74,6 +68,3 @@ int32_t	check_if_redir_built(t_token *tok, t_input *data)
 	}
 	return (ret);
 }
-
-		// else if (tok->type == DLESS)
-		// 	ret = built_redir_dless(tok, data);
